4e18b1710d11711cb78739692a9f0f81aac3a127
ModernAU <92915902+ModernAU@users.noreply.github.com>
2021-10-21 18:56:40 +1030

    Modern Pinger
    This is a pinger I have made, it's very simple to use, no set up or anything, just simply open the .bat file and you're ready to go! If you're going to skid this please give me credit in your version of it.

---
+++ ModernPinger.bat
@@ -0,0 +1,101 @@
+@echo off
+chcp 65001
+title [+] Modern Pinger [+]
+color 4
+
+ECHO                ...
+ECHO              ;::::;
+ECHO            ;::::; :;
+ECHO          ;:::::'   :;
+ECHO         ;:::::;     ;.
+ECHO        ,:::::'       ;           OOO\
+ECHO        ::::::;       ;          OOOOO\
+ECHO        ;:::::;       ;         OOOOOOOO
+ECHO       ,;::::::;     ;'         / OOOOOOO
+ECHO     ;:::::::::`. ,,,;.        /  / DOOOOOO
+ECHO   .';:::::::::::::::::;,     /  /     DOOOO
+ECHO  ,::::::;::::::;;;;::::;,   /  /        DOOO
+ECHO ;`::::::`'::::::;;;::::: ,#/  /          DOOO
+ECHO :`:::::::`;::::::;;::: ;::#  /            DOOO
+ECHO ::`:::::::`;:::::::: ;::::# /              DOO
+ECHO `:`:::::::`;:::::: ;::::::#/               DOO
+ECHO  :::`:::::::`;; ;:::::::::##                OO
+ECHO  ::::`:::::::`;::::::::;:::#                OO
+ECHO  `:::::`::::::::::::;'`:;::#                O
+ECHO   `:::::`::::::::;' /  / `:#
+ECHO    ::::::`:::::;'  /  /   `#                              
+ECHO ███╗   ███╗ ██████╗ ██████╗ ███████╗██████╗ ███╗   ██╗    ██████╗ ██╗███╗   ██╗ ██████╗ ███████╗██████╗ 
+ECHO ████╗ ████║██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗  ██║    ██╔══██╗██║████╗  ██║██╔════╝ ██╔════╝██╔══██╗
+ECHO ██╔████╔██║██║   ██║██║  ██║█████╗  ██████╔╝██╔██╗ ██║    ██████╔╝██║██╔██╗ ██║██║  ███╗█████╗  ██████╔╝
+ECHO ██║╚██╔╝██║██║   ██║██║  ██║██╔══╝  ██╔══██╗██║╚██╗██║    ██╔═══╝ ██║██║╚██╗██║██║   ██║██╔══╝  ██╔══██╗
+ECHO ██║ ╚═╝ ██║╚██████╔╝██████╔╝███████╗██║  ██║██║ ╚████║    ██║     ██║██║ ╚████║╚██████╔╝███████╗██║  ██║
+ECHO ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝                                                                                                                                                                         
+ECHO 
+
+set /p IP=(CTRL + C TO STOP ATTACK) Enter Targets IP:
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+
+color a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo IP Downed Or Not Working.)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
